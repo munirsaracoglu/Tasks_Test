3150ab052cf3d546831b0b2e1822944b49c1c233
munirsaracoglu <munir.saracoglu@toptal.com>
2023-11-03 15:35:21 +0300

    Refactored the code based on the comments

--- tests/api/task1.test.js
+++ tests/api/task1.test.js
@@ -1,43 +1,36 @@
 import {test, expect} from '@playwright/test';
 
 test.describe("API testing", () => {
-
   const baseUrl = "http://localhost:3030/"
-
-  test("Get all Lists with their List Items", async ({request}) => {
-
-    const response = await request.get(`${baseUrl}/lists`)
-    expect(response.status()).toBe(200)
-
-    const responseBody = JSON.parse(await response.text())
-    console.log(responseBody)
+  test("Overwriting the database", async ({request}) => {
+    await request.post(`${baseUrl}/overwrite_database`, {data: '[]'})
   })
-
-  test("Assert Invalid Endpoint for GET request", async ({request}) => {
-
-    const response = await request.get(`${baseUrl}/invalidendpoint`)
-    expect(response.status()).toBe(404)
-  })
-
+  
   test("Add a List with POST request", async ({request}) => {
-
     const response = await request.post(`${baseUrl}/lists/add`, {
       data: {
         name: "Toptal"
       },
     })
-
       const responseBody = JSON.parse(await response.text())
-    // console.log(responseBody)
       expect(responseBody.name).toBe("Toptal")
       expect(responseBody.items).toBeTruthy()
+  }) 
+  
+  test("Get all Lists with their List Items", async ({request}) => {
+    const response = await request.get(`${baseUrl}/lists`)
+    expect(response.status()).toBe(200)
+    const responseBody = JSON.parse(await response.text())
+    expect(responseBody).toBeTruthy()
   })
-
-    test("Delete a List", async ({request}) => {
-
-      const response = await request.delete(`${baseUrl}/lists/4`)
-      expect(response.status()).not.toBe(404) 
+  
+  test("Assert Invalid Endpoint for GET request", async ({request}) => {
+    const response = await request.get(`${baseUrl}/invalidendpoint`)
+    expect(response.status()).toBe(404)
+  })
+  
+  test("Delete a List", async ({request}) => {
+    const response = await request.delete(`${baseUrl}/lists/0`)
+    expect(response.status()).not.toBe(404) 
     })
-
-
 })
